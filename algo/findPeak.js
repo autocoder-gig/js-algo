78e188d833dca0eb5466a8a79e8ab9904b6783cc
Kunal Gupta <2013umt1330@mnit.ac.in>
2016-07-27 23:59:21 +0530

    trying to make a library

--- algo/findPeak.js
+++ algo/findPeak.js
@@ -4,8 +4,8 @@ Problem Statement: Find a peak
 Algo: Brute Force, Divide & Conquer
 Use: node findPeak.js [--bruteForce/divideConquer] <integers seperated by space> | eg.: node findPeak.js --divideConquer 3 4 1 3 5 6 9 
 */
-var arr_length= process.argv.length;
-function Util(param,fun){
+(function(){
+var Util = function(param,fun){
     if(param[0]==='--bruteForce') {
         return fun(mapNumber(param.slice(1,arr_length),function(item){
                 return Number(item);
@@ -25,20 +25,20 @@ function Util(param,fun){
         return "Wrong parameter!\n How to Use:\n node findpeak.js [--bruteForce/divideConquer] <numbers seperated by space>";
     }
 }
-function mapNumber(arr, func){ //Custom function for convertion string array to number
+var mapNumber = function(arr, func){ //Custom function for convertion string array to number array | can use .map
     for(var i =0; i<arr.length; i++){
         arr[i]=func(arr[i]);
     }
     return arr;
 }
-function max(value1, value2) {
+var max = function(value1, value2) {
     if(value1>=value2)
         return value1;
     else
         return value2;
 }
 
-function findPeak(arr, algo) {
+var findPeak = function(arr, algo) {
     var len = arr.length;
     if(len === 0) {
             return;
@@ -76,8 +76,9 @@ function findPeak(arr, algo) {
         }
     }
 }
-
-function greedyAscent(){
-
-}
-console.log(Util(process.argv.slice(2,arr_length),findPeak));
+this.algo = this.$ = {};
+this.algo.findPeak = findPeak;
+this.algo.findPeak.Util = Util
+}(this));
+var arr_length= process.argv.length;
+console.log($.findPeak.Util(process.argv.slice(2,arr_length),$.findPeak));
